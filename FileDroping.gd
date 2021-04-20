4f00f104853b5d31df187df82602926b5658b861
CarbsCode <53490199+CarbsCode@users.noreply.github.com>
2021-04-20 09:37:57 -0700

    Create FileDroping.gd

---
+++ FileDroping.gd
@@ -0,0 +1,13 @@
+func _ready():
+	get_tree().connect("files_dropped",self,"handleDroppedFiles") #This is a signal emitted when you drop a file in to the window.
+
+
+func handleDroppedFiles(_files:PoolStringArray,_screen:int)->void:
+	var dropped_image:Image = Image.new() #Start by creating a new image that will be used for a texture.
+	dropped_image.load(_files[0]) # Take the data from the file and load it it to the image.
+	var new_texture:ImageTexture = ImageTexture.new()  # Create a new texture to apply your image to.
+	new_texture.create_from_image(dropped_image,Texture.FLAG_REPEAT) # Create a texture using the dropped image.
+        applyTexture(new_texture) #apply the texture to the node.
+
+func applyTexture(_texture:ImageTexture) -> void:
+            $nodeToApplyTextureTo.texture =_texture
